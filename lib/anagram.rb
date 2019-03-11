267013e065315dfee34e8fc09cc0670a57eaea73
ryan m <contact@ryanmeek.com>
2019-03-11 15:01:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,6 +2,10 @@
 class Anagram
   attr_accessor :word
   
+  def initialize(word)
+    @word = word
+  end
+  
   def match(words)
     words.each do |word|
       word.split("").all?{|letter| @word.split("")}
